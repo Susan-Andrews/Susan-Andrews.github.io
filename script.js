51108812462c2e9d17a0078324aff78768efdd7c
Susan Andrews <susanandrews2001@gmail.com>
2023-06-06 13:11:48 +0530

    fixes

--- script.js
+++ script.js
@@ -16,3 +16,14 @@
         
 //     });
 // });
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "150px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
