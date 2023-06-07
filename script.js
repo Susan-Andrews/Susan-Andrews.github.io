64a49cfd23d9f07fd43d87eb4f84796365be8b2f
Susan Andrews <susanandrews2001@gmail.com>
2023-06-07 15:43:03 +0530

    navbar js access

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         if(this.scrollY > 20 ){
-//             $('.navbar').addClass('sticky');
-//         }else{
-//             $('.navbar').removeClass('sticky');
-//         }
-
-//     });
-//     // toggle menu
-
-//     $('.menu-btn').click(function(){
-//         $('.navbar .menu').toggleClass("active");
-//         $('.menu-btn i').toggleClass("active");
-
-        
-//     });
-// });
-
-
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "150px";
