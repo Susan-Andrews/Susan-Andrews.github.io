671e45053a9c1be56375fc30a94fc3f4b7a92c9f
Susan Andrews <susanandrews2001@gmail.com>
2023-06-11 14:26:25 +0530

    finalized project section

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
+
+// Owl Carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
