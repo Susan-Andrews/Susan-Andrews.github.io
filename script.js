c55c1eae97db088bdbf343eadb7c11d02fea5df0
Susan Andrews <susanandrews2001@gmail.com>
2023-09-01 15:38:26 +0530

    making boothiful  : )

--- script.js
+++ script.js
@@ -8,31 +8,12 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-
-// Owl Carousel
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:3
-        }
-    }
-})
-
-
 // bottom to top button 
 let mybutton = document.getElementById("btn-back-to-top");
 window.onscroll = function () {
     scrollFunction();
   };
+  
   
 function scrollFunction() {
     if (
